32723215e8725ac027b392627cb7fe7008891ad9
otto-mata <ottomata@proton.me>
2026-01-08 17:58:16 +0100

    feat: add nestjs/config module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule available globally
+      envFilePath: '.env', // Path to your .env file
+      cache: true, // Cache environment variables for better performance
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
