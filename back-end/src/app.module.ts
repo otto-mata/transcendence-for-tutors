c87e6127da31426f000e0bf876b8405d4899fca2
otto-mata <ottomata@proton.me>
2026-01-14 12:49:33 +0100

    fix: Repaired the backend

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { NotificationModule } from './notification/notification.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
+  imports: [JwtModule.register({
+    global: true,
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '15m' },
+  }),
     PrismaModule,
-    ConfigModule.forRoot({
-      isGlobal: true, // Makes ConfigModule available globally
-      envFilePath: '.env', // Path to your .env file
-      cache: true, // Cache environment variables for better performance
-    }),
+    NotificationModule,
+  ConfigModule.forRoot({
+    isGlobal: true, // Makes ConfigModule available globally
+    envFilePath: '.env', // Path to your .env file
+    cache: true, // Cache environment variables for better performance
+  }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ back-end/src/auth/auth.service.ts
@@ -0,0 +1,56 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import {
+	Injectable,
+	UnauthorizedException
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Prisma, User } from '@p/generated/client';
+
+@Injectable()
+export class AuthService {
+	constructor(private prisma: PrismaService,
+		private jwtService: JwtService, private configService: ConfigService) { }
+
+	async createUser(
+		data: Prisma.UserCreateInput
+	): Promise<User> {
+		return this.prisma.user.create({ data });
+	}
+
+	async LoginUser(
+		login: string,
+		password: string
+	): Promise<{ access_token: string }> {
+		const user = await this.prisma.user.findUnique({ where: { login: login } });
+		if (user == null || user['password'] !== password)
+			throw new UnauthorizedException();
+		const payload = {
+			login: user['login']
+		}
+		return {
+			access_token: await this.jwtService.signAsync(payload)
+		};
+	}
+
+	async RefreshToken(
+		token: string
+	): Promise<{ access_token: string }> {
+		try {
+			const data = await this.jwtService.verifyAsync(token, {
+				secret: this.configService.get("JWT_SECRET")
+			});
+			if (!data || !data['password'] || !data['login'])
+				throw new UnauthorizedException();
+			return {
+				access_token: await this.jwtService.signAsync({
+					login: data['login'],
+					password: data['password']
+				})
+			};
+		}
+		catch {
+			throw new UnauthorizedException();
+		}
+	}
+}
