d3d450c403ca3ce40fe682ebc247a5f25a68c523
green <green.reaper987@gmail.com>
2026-01-26 23:22:24 +0100

    fixed double side bar and renamed branch to match the linear name

--- front-end/components/ContentWithSidebar.tsx
+++ front-end/components/ContentWithSidebar.tsx
@@ -1,16 +1,10 @@
-import DesktopSidebar from "./DesktopSidebar";
-
 export const WithSidebar = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  // Sidebar is provided globally by ResponsiveNav in layout; this wrapper only centers content.
   return (
-    <div className="flex justify-center xl:grid xl:grid-cols-[auto_1fr]">
-      <div className="hidden xl:block">
-        <DesktopSidebar />
-      </div>
-      <div className="w-full max-w-3xl mx-auto xl:max-w-6xl">{children}</div>
-    </div>
+    <div className="w-full max-w-3xl mx-auto xl:max-w-6xl">{children}</div>
   );
 };
